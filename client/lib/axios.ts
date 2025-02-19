e7a16306054acb6c1166197a59db19eccf22ddbd
utsavdotdev <95236817+utsavdotdev@users.noreply.github.com>
2025-02-19 23:54:23 +0545

    chore: add hosted api

--- client/lib/axios.ts
+++ client/lib/axios.ts
@@ -1,10 +1,10 @@
 import axios from "axios"
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://used-car-price-prediction-7df1.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
-})
+});
 
 export default instance
